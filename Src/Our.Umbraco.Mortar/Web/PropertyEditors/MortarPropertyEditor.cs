c3610bb4b9698a0866cebbcf8bdf75d03febc6bb
mattbrailsford <me@mattbrailsford.com>
2014-04-21 16:33:00 +0100

    Removed package manifset file and move asset depedencies to PropertyEditorAsset attributes so everything is in one location

--- Src/Our.Umbraco.Mortar/Web/PropertyEditors/MortarPropertyEditor.cs
+++ Src/Our.Umbraco.Mortar/Web/PropertyEditors/MortarPropertyEditor.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Xml.Linq;
+using ClientDependency.Core;
 using Newtonsoft.Json;
 using Our.Umbraco.Mortar.Models;
 using Umbraco.Core;
@@ -10,9 +11,14 @@ using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Core.PropertyEditors;
 using Umbraco.Core.Services;
 using Umbraco.Web.PropertyEditors;
+using Constants = Umbraco.Core.Constants;
 
 namespace Our.Umbraco.Mortar.Web.PropertyEditors
 {
+	[PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/Mortar/Js/mortar.resources.js")]
+	[PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/Mortar/Js/mortar.controllers.js")]
+	[PropertyEditorAsset(ClientDependencyType.Javascript, "/App_Plugins/Mortar/Js/mortar.directives.js")]
+	[PropertyEditorAsset(ClientDependencyType.Css, "/App_Plugins/Mortar/Css/mortar.css")]
 	[PropertyEditor("Our.Umbraco.Mortar", "Mortar", "/App_Plugins/Mortar/Views/mortar.html", 
 		HideLabel = true, ValueType = "JSON")]
 	public class MortarPropertyEditor : PropertyEditor
@@ -63,12 +69,76 @@ namespace Our.Umbraco.Mortar.Web.PropertyEditors
 
 			public override string ConvertDbToString(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
 			{
-				return base.ConvertDbToString(property, propertyType, dataTypeService);
-			}
+				var value = JsonConvert.DeserializeObject<MortarValue>(property.Value.ToString());
 
-			public override XNode ConvertDbToXml(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
-			{
-				return base.ConvertDbToXml(property, propertyType, dataTypeService);
+				foreach (var key in value.Keys)
+				{
+					foreach (var row in value[key])
+					{
+						foreach (var item in row.Items)
+						{
+							if (item != null && !item.RawValue.IsNullOrWhiteSpace())
+							{
+								switch (item.Type.ToLowerInvariant())
+								{
+									case "richtext":
+										// Create a fake DTD
+										var rteDtd = new DataTypeDefinition(-1, Constants.PropertyEditors.TinyMCEAlias);
+
+										// Create a fake property type
+										var rtePropType = new PropertyType(rteDtd) { Alias = "bodyText" };
+
+										// Create a fake property
+										var rteProp = new Property(rtePropType, item.RawValue);
+
+										// Lookup the property editor
+										var rtePropEditor = PropertyEditorResolver.Current.GetByAlias(Constants.PropertyEditors.TinyMCEAlias);
+
+										// Get the editor to do it's conversion, and store the value back
+										item.RawValue = rtePropEditor.ValueEditor.ConvertDbToString(rteProp, rtePropType, dataTypeService);
+										break;
+									case "doctype":
+										if (item.AdditionalInfo.ContainsKey("docType")
+											&& item.AdditionalInfo["docType"] != null
+											&& !item.AdditionalInfo["docType"].IsNullOrWhiteSpace())
+										{
+											// Lookup the doctype
+											var docTypeAlias = item.AdditionalInfo["docType"];
+											var contentType = ApplicationContext.Current.Services.ContentTypeService.GetContentType(docTypeAlias);
+
+											// Loop through properties
+											var propValues = JsonConvert.DeserializeObject<Dictionary<string, object>>(item.RawValue);
+											var propValueKeys = propValues.Keys.ToArray();
+											foreach (var propKey in propValueKeys)
+											{
+												// Lookup the property type on the content type
+												var propType = contentType.PropertyTypes.First(x => x.Alias == propKey);
+
+												// Create a fake property using the property abd stored value
+												var prop = new Property(propType, item.RawValue);
+
+												// Lookup the property editor
+												var propEditor = PropertyEditorResolver.Current.GetByAlias(propType.PropertyEditorAlias);
+
+												// Get the editor to do it's conversion, and store it back
+												propValues[propKey] = propEditor.ValueEditor.ConvertDbToString(prop, propType, dataTypeService);
+											}
+
+											// Serialize the dictionary back
+											item.RawValue = JsonConvert.SerializeObject(propValues);
+										}
+										break;
+								}
+							}
+						}
+					}
+				}
+
+				// Update the value on the property
+				property.Value = JsonConvert.SerializeObject(value);
+
+				// Pass the call down
+				return base.ConvertDbToString(property, propertyType, dataTypeService);
 			}
 
 			public override object ConvertDbToEditor(Property property, PropertyType propertyType, IDataTypeService dataTypeService)
@@ -165,11 +235,8 @@ namespace Our.Umbraco.Mortar.Web.PropertyEditors
 								switch (item.Type.ToLowerInvariant())
 								{
 									case "richtext":
-										// Create a fake property type
-										var rtePropType = new PublishedPropertyType("bodyText", Constants.PropertyEditors.TinyMCEAlias);
-										
 										// Lookup the property editor
-										var rtePropEditor = PropertyEditorResolver.Current.GetByAlias(rtePropType.PropertyEditorAlias);
+										var rtePropEditor = PropertyEditorResolver.Current.GetByAlias(Constants.PropertyEditors.TinyMCEAlias);
 										
 										// Create a fake content property data object (note, we don't have a prevalue, so passing in null)
 										var rteContentPropData = new ContentPropertyData(item.RawValue, null, new Dictionary<string, object>());
