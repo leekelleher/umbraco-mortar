b13cfa4e2b575f3b4ae040769e53bbcc36ac7d5a
leekelleher <leekelleher@gmail.com>
2014-05-21 10:09:52 +0100

    Added DataResolver provider (class file)

---
+++ Src/Our.Umbraco.Mortar/DataResolvers/MortarDataResolver.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Our.Umbraco.Mortar.Web.PropertyEditors;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Courier.Core;
+using Umbraco.Courier.Core.Enums;
+using Umbraco.Courier.Core.Helpers;
+using Umbraco.Courier.DataResolvers;
+using Umbraco.Courier.ItemProviders;
+
+namespace Our.Umbraco.Mortar.DataResolvers
+{
+	public class MortarDataResolver : PropertyDataResolverProvider
+	{
+		private enum Direction
+		{
+			Extracting,
+			Packaging
+		}
+
+		public override string EditorAlias
+		{
+			get
+			{
+				return MortarPropertyEditor.PropertyEditorAlias;
+			}
+		}
+
+		public override void ExtractingDataType(DataType item)
+		{
+			base.ExtractingDataType(item);
+		}
+
+		public override void ExtractingProperty(Item item, ContentProperty propertyData)
+		{
+			base.ExtractingProperty(item, propertyData);
+		}
+
+		public override void PackagingDataType(DataType item)
+		{
+			base.PackagingDataType(item);
+		}
+
+		public override void PackagingProperty(Item item, ContentProperty propertyData)
+		{
+			base.PackagingProperty(item, propertyData);
+		}
+
+		private PublishedPropertyType CreateFakePropertyType(int dataTypeId, string propertyEditorAlias)
+		{
+			return new PublishedPropertyType(null, new PropertyType(new DataTypeDefinition(-1, propertyEditorAlias) { Id = dataTypeId }));
+		}
+	}
+}
